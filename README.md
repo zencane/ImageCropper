ae65f6074cc84db7092a8e2cc575dd4a081c1271
zencane <zenrycane@gmail.com>
2025-11-25 13:23:38 +0000

    launch

---
+++ README.md
@@ -0,0 +1,15 @@
+# Image Cropper!
+
+This project serves the need to easily crop images which is done using [imageCrop.py](imageCrop.py). The state of development is documented below.
+
+## Visuals
+Intended look (design):
+![Intended design](history/cropImagedesign.png)
+
+Previous look (25/11/2025 1):
+![Previous look](history/cropImage251120251143.png)
+
+Current look (25/11/2025 2):
+![Current look](history/cropImage251120251320.png)
+
+The UI and functionality is implemented in [imageCrop.py](imageCrop.py). 
---
+++ imageCrop.py
@@ -0,0 +1,80 @@
+from tkinter import *
+from tkinter import ttk
+from tkinter import filedialog
+from PIL import Image, ImageTk
+
+root = Tk()
+root.title("Image Cropper")
+
+pic = None
+
+
+def upload():
+    global pic
+    fileTypes = [("Image files", "*.png")]
+    path = filedialog.askopenfilename(filetypes=fileTypes)
+    print('Selected:', path)
+    img = Image.open(path)
+    img = img.resize((320, 320))
+    pic = ImageTk.PhotoImage(img)
+    filename.set(path)
+    confirmButton.config(state=NORMAL)
+    #root.geometry("640x640")
+
+def confirm():
+    print("Setting image on label")
+    imageLabel.config(image=pic)
+    imageLabel.image = pic
+    confirmButton.config(state=DISABLED)
+
+def crop():
+    print("Cropping image")
+    #imagelabel.configure(image=upload_image)
+    cropButton.config(state=DISABLED)
+
+def selectVertices():
+    print("Selecting vertices")
+    cropButton.config(state=NORMAL)
+
+windowWidth = 640
+frameWidth = 320
+
+
+mainframe = ttk.Frame(root, padding=(20, 5, 20, 0))
+mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+
+uploadframe = ttk.Frame(mainframe, borderwidth=2, relief="sunken", width=320, height=320)
+uploadframe.grid(column=0, row=2, sticky=(N, W, E, S), columnspan=2)
+
+imageframe = ttk.Frame(mainframe, borderwidth=2, relief="sunken", width=640, height=640)
+imageframe.grid(column=0, row=5, sticky=(N, W, E, S), columnspan=2)
+
+tempImage = ImageTk.PhotoImage(Image.open('upload.png').resize((320, 320)))
+filename = StringVar()
+filename.set("No file selected")
+
+
+titleLabel = ttk.Label(mainframe, text="Image Cropper", font=("Helvetica", 16))
+fileLabel = ttk.Label(uploadframe, textvariable=filename, wraplength=320, font=("Helvetica", 8))
+uploadButton = ttk.Button(mainframe, text="Upload", command=lambda:upload())
+confirmButton = ttk.Button(mainframe, text="Confirm", state=DISABLED, command=lambda:confirm())
+imageLabel = ttk.Label(imageframe, image=tempImage, anchor=SE)
+cropButton = ttk.Button(mainframe, text="Crop", state=DISABLED, command=lambda:crop())
+
+
+titleLabel.grid(column=0, row=1, sticky=N, columnspan=2)
+fileLabel.grid(column=0, row=0, sticky=N)
+uploadButton.grid(column=0, row=3, sticky=E)
+confirmButton.grid(column=1, row=3, sticky=W)
+imageLabel.grid(column=0, row=0, sticky=(N, W, E, S))
+cropButton.grid(column=0, row=6, sticky=N, columnspan=2)
+
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)	
+mainframe.columnconfigure(2, weight=1)
+imageframe.columnconfigure(3, weight=1)
+
+for child in mainframe.winfo_children(): 
+    child.grid_configure(padx=10, pady=5)
+
+root.mainloop()
